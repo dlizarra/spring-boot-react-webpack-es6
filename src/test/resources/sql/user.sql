bf69014b0ae9d2573d123db2b38a2c79c22f3797
David Lizárraga <dlizarra5@gmail.com>
2015-10-12 04:29:37 +0200

    Updated user repository test sql script

--- src/test/resources/sql/user.sql
+++ src/test/resources/sql/user.sql
@@ -1,2 +1,8 @@
--- User
-INSERT INTO "users"("id","username") VALUES (7, 'test');
+-- project
+INSERT INTO "project"("id","name") VALUES (1, 'Project1');
+INSERT INTO "project"("id","name") VALUES (2, 'Project2');
+-- users
+INSERT INTO "users"("id","username") VALUES (1, 'david');
+-- project2User2Position
+INSERT INTO "project2user2position"("project_id", "user_id", "position_id") VALUES (1,1,1);
+INSERT INTO "project2user2position"("project_id", "user_id", "position_id") VALUES (2,1,2);
