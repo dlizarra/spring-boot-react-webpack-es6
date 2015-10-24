123a82efe81fd65ee46f9037e7b25948a2456bce
David Lizárraga <dlizarra5@gmail.com>
2015-10-24 20:54:44 +0200

    Fixed wrong pushed folders

--- src/main%C0java/com/dlizarra/startuphub/project/ProjectNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.dlizarra.startuphub.project;
-
-@SuppressWarnings("serial")
-public class ProjectNotFoundException extends RuntimeException {
-
-	public ProjectNotFoundException(final Integer id) {
-		super("Could not find Project with id: " + id);
-	}
-}
