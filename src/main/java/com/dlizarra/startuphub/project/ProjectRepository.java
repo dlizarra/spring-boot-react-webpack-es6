6a35a87718bf18c232347e42f782a65ca901bec2
David Lizárraga <dlizarra5@gmail.com>
2016-02-25 23:42:20 +0100

    Added @Repository so Idea stops complaining that theres no implementation class for this interface

--- src/main/java/com/dlizarra/startuphub/project/ProjectRepository.java
+++ src/main/java/com/dlizarra/startuphub/project/ProjectRepository.java
@@ -1,7 +1,9 @@
 package com.dlizarra.startuphub.project;
 
 import com.dlizarra.startuphub.support.jpa.CustomJpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProjectRepository extends CustomJpaRepository<Project, Integer> {
 
 }
--- src/main/java/com/dlizarra/startuphub/user/UserRepository.java
+++ src/main/java/com/dlizarra/startuphub/user/UserRepository.java
@@ -3,7 +3,9 @@ package com.dlizarra.startuphub.user;
 import java.util.Optional;
 
 import com.dlizarra.startuphub.support.jpa.CustomJpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CustomJpaRepository<User, Integer> {
 
 	Optional<User> findByUsername(String username);
