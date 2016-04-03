bea19275ec7689bcf52fff44625534c5fcbd4e57
David Lizárraga <dlizarra5@gmail.com>
2016-04-03 16:44:31 +0200

    cleaned up config code

--- src/main/frontend/webpack.config.js
+++ src/main/frontend/webpack.config.js
@@ -1,38 +1,54 @@
-var path = require("path");
-var webpack = require('webpack');
+const path = require('path');
+const merge = require('webpack-merge');
 
-module.exports = {
-    entry: [
-        "./app/index.js"
-    ],
-    output: {
-        path: path.resolve(__dirname, '../../../target/classes/static'),
-        publicPath: '',
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [{
-            exclude: /node_modules/,
-            loader: 'babel'
-        }, {
-            test: /\.css$/,
-            loader: 'style!css'
-        }]
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  source: path.join(__dirname, 'app'),
+  output: path.join(__dirname, '../../../target/classes/static')
+};
+
+const common = {
+  entry: [
+    PATHS.source
+  ],
+  output: {
+    path: PATHS.output,
+    publicPath: '',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel'
+    }, {
+      test: /\.css$/,
+      loader: 'style!css'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+};
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
     devServer: {
-        port: 9090,
-        proxy: {
-            '/*': {
-                target: 'http://localhost:8080',
-                secure: false,
-                prependPath: false
-            }
-        },
-        publicPath: 'http://localhost:9090/',
-        historyApiFallback: true
+      port: 9090,
+      proxy: {
+        '/*': {
+          target: 'http://localhost:8080',
+          secure: false,
+          prependPath: false
+        }
+      },
+      publicPath: 'http://localhost:9090/',
+      historyApiFallback: true
     },
     devtool: 'source-map'
-};
+  });
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {});
+}
+
